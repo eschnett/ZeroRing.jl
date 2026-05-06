7b112ea3a69694c8edd81370cd0ad1a585a230a4
Erik Schnetter <schnetter@gmail.com>
2026-05-06 14:38:20 -0400

    Disable division; new version 2.0

--- src/ZeroRing.jl
+++ src/ZeroRing.jl
@@ -10,22 +10,30 @@ struct ZeroElem <: Number end
 const zeroelem = ZeroElem()
 
 Base.:(==)(::ZeroElem, ::ZeroElem) = true
-Base.:(<)(::ZeroElem, ::ZeroElem) = false
+Base.isless(::ZeroElem, ::ZeroElem) = false
+Base.iszero(::ZeroElem) = true
 Base.hash(::ZeroElem, h::UInt) = hash(UInt(0x56212a80), h)
 
+Base.zero(::ZeroElem) = zeroelem
+Base.zero(::Type{ZeroElem}) = zeroelem
 Base.rand(rng::AbstractRNG, ::Random.SamplerType{ZeroElem}) = zeroelem
+
+Base.abs(::ZeroElem) = zeroelem
+Base.abs2(::ZeroElem) = zeroelem
+Base.conj(::ZeroElem) = zeroelem
+Base.sign(::ZeroElem) = zeroelem
 
 Base.:+(::ZeroElem) = zeroelem
 Base.:-(::ZeroElem) = zeroelem
 
-Base.:+(::ZeroElem, ::ZeroElem) = zeroelem
+Base.:+(::ZeroElem, ::ZeroElem...) = zeroelem
 Base.:-(::ZeroElem, ::ZeroElem) = zeroelem
 
-Base.inv(::ZeroElem) = zeroelem
+# Base.inv(::ZeroElem) = zeroelem
 
-Base.:*(::ZeroElem, ::ZeroElem) = zeroelem
-Base.:/(::ZeroElem, ::ZeroElem) = zeroelem
-Base.:\(::ZeroElem, ::ZeroElem) = zeroelem
+Base.:*(::ZeroElem, ::ZeroElem...) = zeroelem
+# Base.:/(::ZeroElem, ::ZeroElem) = zeroelem
+# Base.:\(::ZeroElem, ::ZeroElem) = zeroelem
 
 Base.:^(::ZeroElem, ::ZeroElem) = zeroelem
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,25 +3,38 @@ using ZeroRing
 
 zeroelem::ZeroElem
 
+@test sizeof([zeroelem]) == 0
+
 @test zeroelem === zeroelem
 
 @test zeroelem == zeroelem
 @test !(zeroelem != zeroelem)
 @test !(zeroelem < zeroelem)
 @test hash(zeroelem) === hash(zeroelem)
+@test iszero(zeroelem)
 
+@test zero(zeroelem) === zeroelem
+@test zero(ZeroElem) === zeroelem
 @test rand(ZeroElem) === zeroelem
+
+@test abs(zeroelem) == zeroelem
+@test abs2(zeroelem) == zeroelem
+@test conj(zeroelem) == zeroelem
+@test sign(zeroelem) == zeroelem
 
 @test +zeroelem === zeroelem
 @test -zeroelem === zeroelem
 
 @test zeroelem + zeroelem === zeroelem
+@test zeroelem + zeroelem + zeroelem === zeroelem
 @test zeroelem - zeroelem === zeroelem
 
-@test inv(zeroelem) === zeroelem
+# @test inv(zeroelem) === zeroelem
 
 @test zeroelem * zeroelem === zeroelem
-@test zeroelem / zeroelem === zeroelem
+@test zeroelem * zeroelem * zeroelem === zeroelem
+# @test zeroelem / zeroelem === zeroelem
+# @test zeroelem \ zeroelem === zeroelem
 
 @test zeroelem^zeroelem === zeroelem
 
